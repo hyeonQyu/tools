e62c608a537f47a7fb3198ebaae86b0464fddc66
hyeonQyu <dhk0561@naver.com>
2026-01-24 02:37:55 +0900

    feat: Provider로 감싸기

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './app/App.tsx'
+import { IndexedDBProvider } from '@/indexed-db';
+import { ThemeProvider } from '@/styles';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <IndexedDBProvider>
+          <App />
+        </IndexedDBProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
