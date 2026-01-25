9f6a007b5f776637ff7da52e2f2c6ee56f05c008
hyeonQyu <dhk0561@naver.com>
2026-01-25 18:38:04 +0900

    refactor: import문에서 확장자명 제거

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from '@/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
