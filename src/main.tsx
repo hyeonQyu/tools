9cccb5437616ab28defc813f6307503341f044f4
hyeonQyu <dhk0561@naver.com>
2026-02-14 17:56:19 +0900

    build: 배포 시 PWA 앱에 자동 반영 되도록 설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,12 @@ import { ThemeProvider } from '@/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App';
+
+registerSW({
+  immediate: true,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,9 @@ export default defineConfig({
         ],
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+        globPatterns: ['**/*.{js,css,ico,png,svg,woff,woff2}'],
+        skipWaiting: true,
+        clientsClaim: true,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
